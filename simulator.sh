58f899caab4680bcc491dddd914410df74b9bf35
shankar <shankarvpatil1599@gmail.com>
2021-06-02 21:41:19 +0530

    Showing if its tie then continue

--- simulator.sh
+++ simulator.sh
@@ -14,8 +14,19 @@ do
    if [ $headswin -eq $tailswin ]
    then
       echo "It's a tie "
+      while [ $(($headswin-$tailswin)) -le 2 ]
+      do
+         toss=$((RANDOM%2))
+         if [ $toss -eq $heads ]
+         then
+            ((headswin++))
+         else
+            ((tailswin++))
+         fi
+      done
       break
-   fi
+      fi
+
 done
 echo "Heads win $headswin times"
 echo "Tails win $tailswin times"
